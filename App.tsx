f2ba1e9ac4fa2a46402128c6d23dbeaeb0c98ffc
[dyad] <git@dyad.sh>
2026-01-06 23:22:48 -0300

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -27,9 +27,9 @@ const defaultIcon = L.icon({
 L.Marker.prototype.options.icon = defaultIcon;
 
 const planeIcon = L.divIcon({
-  html: `<div style="transform: rotate(var(--heading, 0deg)); transition: transform 0.5s ease-out;">
+  html: `<div style="transform: rotate(0deg); transition: transform 0.5s;">
           <svg width="40" height="40" viewBox="0 0 24 24" fill="#a855f7" stroke="white" stroke-width="0.5">
-            <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/>
+            <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/>
           </svg>
         </div>`,
   className: 'plane-icon',
